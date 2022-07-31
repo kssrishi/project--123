24311493d40c1985307abdd40fa69ddd8589f97f
kssrishi <92584456+kssrishi@users.noreply.github.com>
2022-07-31 17:22:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500)
     canvas = createCanvas(550, 550)
-    canvas.position(560, 150)
+    canvas.position(560, 110)
     poseNet = ml5.poseNet(video, modelLoaed)
     poseNet.on('pose',gotPoses)
 }
@@ -15,8 +15,6 @@ function modelLoaed() {
 function gotPoses(results) {
     if (results.length>0) {
         console.log(results) 
-        noseX = results[0].pose.nose.x
-        noseY = results[0].pose.nose.y;
         leftWristX = results[0].pose.leftWrist.x
         rightWristX = results[0].pose.rightWrist.x;
         difference=floor(leftWristX-rightWristX)
@@ -24,8 +22,8 @@ function gotPoses(results) {
 }
 function draw() {
     background("white")
-    document.getElementById("").innerHTML = "font size of the text will be = " + difference + " px ";
+    document.getElementById("font_size").innerHTML = "font size of the text will be = " + difference + " px ";
     fill("green")
-    stroke("green")
-    square(noseX,noseY,difference)
+    textSize(difference);
+    text("HELLO",200,209)
 }
